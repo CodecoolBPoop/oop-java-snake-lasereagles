6353b8f6bd8995d9bd083a30d9c3f7cbd92ae1df
“elmerzsolt” <drogentm@gmail.com>
2018-07-24 16:46:37 +0200

    Menubar is created.
    Exit game implemented.
    
    New game is not implemented.

--- src/com/codecool/snake/Main.java
+++ src/com/codecool/snake/Main.java
@@ -2,6 +2,7 @@ package com.codecool.snake;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -12,10 +13,15 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) {
+
         Game game = new Game();
 
+        BorderPane root = new BorderPane();
+        root.setTop(MenuFactory.getMenuBar());
+        root.setBottom(game);
+
         primaryStage.setTitle("Snake Game");
-        primaryStage.setScene(new Scene(game, Globals.WINDOW_WIDTH, Globals.WINDOW_HEIGHT));
+        primaryStage.setScene(new Scene(root, Globals.WINDOW_WIDTH, Globals.WINDOW_HEIGHT));
         primaryStage.show();
         game.start();
     }
