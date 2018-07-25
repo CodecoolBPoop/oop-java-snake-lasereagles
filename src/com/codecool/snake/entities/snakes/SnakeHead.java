76e19c350f88106e5773541ce8d82f728425d1be
davidlenner <david910128@gmail.com>
2018-07-25 16:15:22 +0200

    all powerups added to game setup

--- src/com/codecool/snake/entities/snakes/SnakeHead.java
+++ src/com/codecool/snake/entities/snakes/SnakeHead.java
@@ -5,11 +5,8 @@ import com.codecool.snake.Globals;
 import com.codecool.snake.entities.Animatable;
 import com.codecool.snake.Utils;
 import com.codecool.snake.entities.Interactable;
-import com.codecool.snake.entities.enemies.Enemy;
-import com.codecool.snake.entities.enemies.SimpleEnemy;
 import javafx.geometry.Point2D;
 import javafx.scene.layout.Pane;
-
 import java.util.ArrayList;
 import java.util.List;
 
@@ -78,6 +75,8 @@ public class SnakeHead extends GameEntity implements Animatable {
         if (isOutOfBounds() || health <= 0) {
             System.out.println("Game Over");
             Globals.gameLoop.stop();
+
+
         }
     }
 
