beac897b3765b40ee8ef0ffff4cc4a6b1c150ca7
danielszakacs <daniel.sokach@gmail.com>
2018-07-25 11:04:48 +0200

    fixed

--- src/com/codecool/snake/entities/enemies/Facebook.java
+++ src/com/codecool/snake/entities/enemies/Facebook.java
@@ -1,8 +1,6 @@
 package com.codecool.snake.entities.enemies;
 
 import com.codecool.snake.Globals;
-import com.codecool.snake.entities.Interactable;
-import com.codecool.snake.entities.snakes.SnakeHead;
 import javafx.scene.layout.Pane;
 
 public class Facebook extends Enemy {
@@ -13,7 +11,7 @@ public class Facebook extends Enemy {
         setImage(Globals.facebook);
         pane.getChildren().add(this);
         this.setSpeed(1);
-        this.setPosition();
+        this.setSpawnPos();
         this.setDirection();
     }
 }
