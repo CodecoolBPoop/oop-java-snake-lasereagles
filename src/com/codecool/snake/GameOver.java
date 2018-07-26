4c5e9dad4aa6c66c5c1675d011e010fec223ab10
davidlenner <david910128@gmail.com>
2018-07-26 13:09:28 +0200

    button added again to popud window but not implemented

--- src/com/codecool/snake/GameOver.java
+++ src/com/codecool/snake/GameOver.java
@@ -1,5 +1,6 @@
 package com.codecool.snake;
 
+import com.sun.xml.internal.bind.v2.TODO;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.layout.VBox;
@@ -21,12 +22,15 @@ public class GameOver {
         window.setTitle(title);
         window.setMinWidth(400);
 
+        Button restartButton = new Button("Restart");
+        // TODO button onAction need to be implemented
+
         Label label = new Label();
         String StrHighScore = Integer.toString(highscore);
         label.setText("Your HighScore is " + StrHighScore);
 
         VBox layout = new VBox(10);
-        layout.getChildren().addAll(label);
+        layout.getChildren().addAll(label, restartButton);
         layout.setAlignment(Pos.CENTER);
 
         Scene scene = new Scene(layout);
