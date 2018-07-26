038e00444b762851d86c9d33df2eedecbd394ba6
davidlenner <david910128@gmail.com>
2018-07-26 11:09:24 +0200

    Merge branch 'develop' of https://github.com/CodecoolBPoop/oop-java-snake-lasereagles into gameover
    # Conflicts:
    #	src/com/codecool/snake/entities/snakes/SnakeHead.java
    
    gameover popup window implemented, but after popup we cant click menubar

---
+++ src/com/codecool/snake/GameOver.java
@@ -0,0 +1,40 @@
+package com.codecool.snake;
+
+
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.layout.VBox;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import javafx.scene.control.*;
+
+
+
+public class GameOver {
+
+    public static void gameover(String title, String message) {
+
+        Stage window = new Stage();
+
+        window.initModality(Modality.APPLICATION_MODAL);
+        window.setTitle(title);
+        window.setMinWidth(250);
+
+        Label label = new Label();
+        label.setText(message);
+        Button button = new Button("close the window");
+
+        button.setOnAction(e -> window.close());
+
+        VBox layout = new VBox(10);
+        layout.getChildren().addAll(label,button);
+        layout.setAlignment(Pos.CENTER);
+
+        Scene scene = new Scene(layout);
+        window.setScene(scene);
+        window.show();
+
+
+
+    }
+}
